8ae328e611d47db4d44e6ff82fe3053332422314
mkurzyna <kurzynama@office.umcs.pl>
2025-06-02 14:25:08 +0200

    threads

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
